12702bb21dcc92fcf3abd54b8276527d698411e9
Anika Luo <al8@wellesley.edu>
2022-05-05 20:38:07 -0400

    get distance/speed from position

--- 22spring/test3a/test3a.jl
+++ 22spring/test3a/test3a.jl
@@ -6,33 +6,36 @@ using Statistics
 using Distances
 
 
-# # CALCULATE SPEED FROM DISTANCE
+# CALCULATE SPEED/DISTANCE FROM POSITION
 
-positiondf = DataFrame(CSV.File("./22spring/test3a/data/Position/74Position.csv", header=5)) # import position csv into df
+df = DataFrame(CSV.File("./22spring/test3a/data/Position/74Position.csv", header=5)) # import position csv into df
 
 divby1(num) = num%1 == 0
-filter!(:Time => divby1, positiondf) # filter df down to position measurements every sec
+filter!(:Time => divby1, df) # filter df down to position measurements every sec
 
-select!(positiondf, Not([1,2])) # delete Frame and Time columns 
+select!(df, Not([:Frame, :Time])) # delete Frame and Time columns 
 
-x1 = positiondf[1, "1 x"]
-y1 = positiondf[1, "1 y"]
-x2 = positiondf[2, "1 x"]
-y2 = positiondf[2, "1 y"]
-euclidean((x1, y1), (x2, y2)) # calculate distance between 2 pts
+df.track = map(eachrow(df)) do row
+    ceil(Int, findfirst(!ismissing, values(row)) / 2)
+end # add column to df with track numbers
 
-track1df = positiondf[:, ["1 x", "1 y"]] # create new df with single track
-dropmissing!(track1df) # remove all rows that say missing
+df.x = map(eachrow(df)) do row
+    coalesce(row[r"\d x"]...) # regex: "\d" = any digit, " " = space, "x" = "x" char
+end # add column to df with all x values
 
-# track1matrix = Matrix(track1df) # create matrix from df
-# track1vectors = DataFrame()
-# for row in eachrow(track1matrix)
-#     append!(track1vectors, row)
-# end
+df.y = map(eachrow(df)) do row
+    coalesce(row[r"\d y"]...)
+end # add column to df with all y values
 
-t1coordinates = DataFrame(coordinate = Vector[])
-for row in eachrow(track1df)
-    coordinate = (row."1 x", row."1 y")
-    println(coordinate)
-    append!(t1vectors, coordinate)
-end
+select!(df, [:track, :x, :y]) # only keep new columns :track, :x, :y
+
+df.speed = map(1:nrow(df)) do ri 
+    if ri == 1 || df.track[ri] != df.track[ri-1] # if first row in csv or track
+        return missing # don't calculate distance
+    else # calculate distance between point in row to point in next row
+        return euclidean([df.x[ri-1], df.x[ri]], [df.y[ri-1], df.y[ri]]) 
+    end
+end # add column to df with distances --> since distance is being measured across 1sec, distance = speed
+
+select!(df, [:track, :speed])
+dropmissing!(df)
