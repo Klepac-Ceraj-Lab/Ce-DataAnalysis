3e6a38ee5319fc23d822d43abcfd2978a822be41
Anika Luo <al8@wellesley.edu>
2022-06-03 17:41:31 -0400

    add functions to module

--- 22spring/experiment4/experiment4b/experiment4b.jl
+++ 22spring/experiment4/experiment4b/experiment4b.jl
@@ -5,45 +5,12 @@ using GLMakie
 using Statistics
 using Distances
 
+using CeDataAnalysis
+using DataFrames
+using CSV
 
 
-# COMPILE DATAFRAME WITH ALL POSITIONS
-
-# TAKE 5 POSITION MEASUREMENTS EVERY SEC -> AVERAGE SPEED ACROSS A SEC
-function load_tracks!(existingdf, file, id)
-    tracks = DataFrame(CSV.File(file, header=5)) # import CSV to DataFrame
-
-    filter!(:Frame=> (num-> num % 5 == 0), tracks) # filter df down to 5 position measurements every sec
-
-    select!(tracks, Not([:Frame, :Time])) # delete Frame and Time columns 
-    
-    ntracks = ncol(tracks) ÷ 2 # count every pair of columns (this is integer division, so the answer stays Int)
-    
-    for tr in 1:ntracks
-        x = collect(skipmissing(tracks[!, 2*tr-1]))
-        y = collect(skipmissing(tracks[!, 2*tr]))
-        
-        if isempty(existingdf) # if there is no data
-            tracknum = 1 # start from 1
-        elseif id == last(existingdf.id) # if the condition is the same as the last
-            tracknum = last(existingdf.track) + 1 # count up from there
-        else # if the condition is not the same
-            tracknum = 1 # start from 1
-        end # assign appropriate track number
-    
-        append!(existingdf, DataFrame(
-            id = fill(id, length(x)),
-            track = fill(tracknum, length(x)),
-            xpos  = x,
-            ypos  = y)
-        ) # add x y coordinates positions of each track to existingdf
-    end
-    
-    return existingdf
-end
-
-
-
+# COMPILE DATAFRAME WITH 5 POSITION MEASUREMENTS EVERY SEC
 data = DataFrame(id=String[], track=Int[], xpos=Float64[], ypos=Float64[])
 
 datafolder = "./22spring/experiment4/experiment4b/data/Position/"
@@ -99,48 +66,10 @@ end
 
 
 # CALCULATE SPEED/DISTANCE FROM POSITION
-
-function speed(df)
-    df.speed = map(1:nrow(df)) do ri 
-        if ri == 1 || df.id[ri] != df.id[ri-1] || df.track[ri] != df.track[ri-1] # if first row in dataframe or condition (id) or track
-            return missing # don't calculate distance
-        else # calculate distance between point in row to point in previous row
-            return euclidean([df.xpos[ri-1], df.xpos[ri]], [df.ypos[ri-1], df.ypos[ri]]) 
-        end
-    end # add column to df with distances --> since distance is being measured across 1sec, distance = speed
-
-    dropmissing!(df) # delete rows with 'missing' speeds
-
-    return df
-end
-
 speed(data) # calculate speed from position
 
-select!(data, [:id, :track, :speed]) # filter out :xpos and :ypos columns
-
-
-
-# AVERAGE 5 MEASUREMENTS TO GET AVERAGE SPEED ACROSS A SEC
-speedpersec = DataFrame(id=String[], track=Int[], speed=Float64[])
-
-for row in 5:5:nrow(data)
-    if data.id[row] == data.id[row-4] && data.track[row] == data.track[row-4]
-        meanspeed = mean([data.speed[row-4], data.speed[row-3], data.speed[row-2], data.speed[row-1], data.speed[row]])
-        append!(speedpersec, DataFrame(id = data.id[row], track = data.track[row], speed = meanspeed))
-    end
-end
-
-
-
-# AVERAGE EVERY 5 SPEED/SEC MEASUREMENTS
-speedperfive = DataFrame(id=String[], track=Int[], speed=Float64[])
-
-for row in 5:5:nrow(speedpersec)
-    if speedpersec.id[row] == speedpersec.id[row-4] && speedpersec.track[row] == speedpersec.track[row-4]
-        meanspeed = mean([speedpersec.speed[row-4], speedpersec.speed[row-3], speedpersec.speed[row-2], speedpersec.speed[row-1], speedpersec.speed[row]])
-        append!(speedperfive, DataFrame(id = speedpersec.id[row], track = speedpersec.track[row], speed = meanspeed))
-    end
-end
+# AVERAGE SPEED MEASUREMENTS ACROSS 5 SEC
+speedperfive = averageoverfive(data)
 
 
 
@@ -152,20 +81,9 @@ speeds = DataFrame(CSV.File("./22spring/experiment4/experiment4b/speeds.csv"))
 
 
 
-# STATS
-
-# individual track stats
-tracks = groupby(speeds, [:id, :track])
-trackstats = combine(tracks, :speed => mean => :meanspeed, :speed => std => :stdspeed)
-
-# condition stats from individual stats 
-conditions = groupby(trackstats, [:id])
-conditionstats = combine(conditions, :meanspeed => mean => :meanofmeanspeed, :meanspeed => std => :stdofmeanspeed, :stdspeed => mean => :meanofstdspeed, :stdspeed => std => :stdofstdspeed)
-
-# condition stats from all data
-all = groupby(speeds, [:id])
-allstats = combine(all, :speed => mean => :meanspeed, :speed => std => :stdspeed)
-
+# SUMMARY STATS
+conditionstats(speeds)
+allstats(speeds)
 
 
 # MAKE CATEGORICAL ARRAYS FOR PLOTTING
--- src/othercode.jl
+++
@@ -1,5 +0,0 @@
-# organize your code in files, then "include" them from the main files
-
-function this_uses_bar(add)
-    return bar() * " And $add"
-end
