38c4c37ed917eee791da89413eb809d6b030f72e
Anika Luo <al8@wellesley.edu>
2022-06-07 17:15:36 -0400

    make leveled CategoricalArrays

--- 22spring/experiment4/experiment4b/experiment4b_plotting.jl
+++ 22spring/experiment4/experiment4b/experiment4b_plotting.jl
@@ -13,12 +13,6 @@ speeds = DataFrame(CSV.File(speedscsv))
 
 
 # MAKE CATEGORICAL ARRAYS FOR PLOTTING
-speeds.medium = categorical(map(i-> split(i, '_')[1], speeds.id))
-speeds.worm = categorical(map(i-> split(i, '_')[2], speeds.id))
-speeds.bacteria = categorical(map(i-> split(i, '_')[3], speeds.id))
-speeds.id = categorical(speeds.id, levels=[ "DA_N2_OP50", "DA_N2_NGM",
-                                            "DA_CB_OP50", "DA_CB_NGM",
-                                            "DA_MT_OP50", "DA_MT_NGM",
-                                            "M9_N2_OP50", "M9_N2_NGM",
-                                            "M9_CB_OP50", "M9_CB_NGM",
-                                            "M9_MT_OP50", "M9_MT_NGM"])
+speeds.medium = categorical(map(i-> split(i, '_')[1], speeds.id), levels = ["M9", "DA"])
+speeds.worm = categorical(map(i-> split(i, '_')[2], speeds.id), levels = ["N2", "CB", "MT"])
+speeds.bacteria = categorical(map(i-> split(i, '_')[3], speeds.id), levels = ["NGM", "OP50"])
