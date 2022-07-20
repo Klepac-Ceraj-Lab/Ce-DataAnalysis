f2d19cf82db4abc1a0ab723b66b3aa2373156655
Anika Luo <al8@wellesley.edu>
2022-07-19 20:59:04 -0400

    separate 5 analysis and plots, save combined csv

--- dopamine/experiment5/experiment5_paperplots.jl
+++ dopamine/experiment5/experiment5_paperplots.jl
@@ -7,21 +7,10 @@ using StatsBase
 
 
 
-# LOAD ALL SPEEDS CSVS AND COMBINE INTO ONE DATAFRAME
+# LOAD SPEEDS CSV INTO DATAFRAME
 experimentdir = @__DIR__
 
-speedsa = DataFrame(CSV.File("./dopamine/experiment5/experiment5a/speeds.csv"))
-speedsa.experiment = fill(1, length(speedsa.id)) # add experiment column to df
-
-speedsb = DataFrame(CSV.File("./dopamine/experiment5/experiment5b/speeds.csv"))
-speedsb.experiment = fill(2, length(speedsb.id))
-
-speedsc = DataFrame(CSV.File("./dopamine/experiment5/experiment5c/speeds.csv"))
-speedsc.experiment = fill(3, length(speedsc.id))
-
-speeds = vcat(speedsa, speedsb, speedsc)
-
-
+speeds = DataFrame(CSV.File(joinpath(experimentdir, "speeds.csv")))
 
 # CALCULATE SUMMARY STATS BY FIRST CALCULATING SUMMARY STATS OF EACH TRACK
 # individual track stats
